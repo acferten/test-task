05688448efafc0d9a5138e42ff6b377a6e6546a8
acferten <gowiththeirname@gmail.com>
2022-10-23 22:59:30 +0700

    Добавлен Docker и json коллекция

--- simpletobuy/api/models.py
+++ simpletobuy/api/models.py
@@ -22,17 +22,15 @@ class Product(models.Model):
         return self.name
 
 
-# class Cart(models.Model):
-#     user = models.ForeignKey(AdvUser, on_delete=models.CASCADE, null=True, blank=False)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False)
-#     name = models.CharField(max_length=250)
-#     description = models.CharField(max_length=3000)
-#     price = models.DecimalField(max_digits=8, decimal_places=2)
+class Cart(models.Model):
+    user = models.ForeignKey(AdvUser, on_delete=models.CASCADE, null=True, blank=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False)
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=3000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
-    # quantity = models.IntegerField(null=False, default=1)
-
-    # def __str__(self):
-    #     return str(self.id)
+    def __str__(self):
+        return str(self.id)
 
 
 class Order(models.Model):
@@ -45,34 +43,3 @@ class OrderItem(models.Model):
     user = models.ForeignKey(AdvUser, on_delete=models.SET_NULL, null=True, blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     amount = models.IntegerField(blank=False)
-
-
-class Cart(models.Model):
-    user = models.ForeignKey(AdvUser, on_delete=models.CASCADE)
-    ordered = models.BooleanField(default=False)
-    total_price = models.FloatField(default=0)
-
-    def __str__(self):
-        return str(self.user.email) + " " + str(self.total_price)
-
-
-class CartItems(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    user = models.ForeignKey(AdvUser, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.FloatField(default=0)
-    quantity = models.IntegerField(default=1)
-
-    def __str__(self):
-        return str(self.user.username) + " " + str(self.product.product_name)
-
-
-@receiver(pre_save, sender=CartItems)
-def correct_price(sender, **kwargs):
-    cart_items = kwargs['instance']
-    price_of_product = Product.objects.get(id=cart_items.product.id)
-    cart_items.price = cart_items.quantity * float(price_of_product.price)
-    total_cart_items = CartItems.objects.filter(user=cart_items.user)
-    cart = Cart.objects.get(id=cart_items.cart.id)
-    cart.total_price = cart_items.price
-    cart.save()
